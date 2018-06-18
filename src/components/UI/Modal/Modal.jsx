de294bee859c0eaf3cd22a1c67115046ff321e21
Amr A. Mohamed <merodiro@gmail.com>
2018-06-18 07:18:22 +0200

    use shouldComponentUpdate

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,23 +1,30 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import PropTypes from 'prop-types'
 
 import classes from './Modal.module.css'
 import Backdrop from '../Backdrop/Backdrop'
 
-const Modal = ({ show, closed, children }) => {
-  let modalClasses = [classes.Modal]
-  show && modalClasses.push(classes.Opened)
-  return (
-    <Fragment>
-      <Backdrop show={show} clicked={closed} />
-      <div className={modalClasses.join(' ')}>{children}</div>
-    </Fragment>
-  )
-}
+export class Modal extends Component {
+  static propTypes = {
+    closed: PropTypes.func,
+    show: PropTypes.bool,
+  }
 
-Modal.propTypes = {
-  closed: PropTypes.func,
-  show: PropTypes.bool,
+  shouldComponentUpdate = nextProps => {
+    return nextProps.show !== this.props.show
+  }
+
+  render() {
+    let { show, closed, children } = this.props
+    let modalClasses = [classes.Modal]
+    show && modalClasses.push(classes.Opened)
+    return (
+      <Fragment>
+        <Backdrop show={show} clicked={closed} />
+        <div className={modalClasses.join(' ')}>{children}</div>
+      </Fragment>
+    )
+  }
 }
 
 export default Modal
